249e93a9368382648bdb9ca851603cf9ae70a24d
InDieTasten <indietasten@gmail.com>
2019-02-03 20:31:41 +0100

    Add sample plugin

---
+++ DevTease.Plugins.PluginA/Plugin.cs
@@ -0,0 +1,11 @@
+﻿using DevTease.Plugins.Api;
+using System;
+
+namespace DevTease.Plugins.PluginA
+{
+    public class Plugin : IPlugin
+    {
+        public void Load() => Console.WriteLine("Plugin A was loaded. Yay!");
+        public void Unload() => Console.WriteLine("Plugin A was unloaded. Nay!");
+    }
+}
